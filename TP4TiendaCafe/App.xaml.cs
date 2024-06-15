792be2152e1dcc994528c49aa3dca180422c54aa
cristianvaldezbideplan <49735677+cristianvaldezbideplan@users.noreply.github.com>
2024-06-15 17:48:15 -0300

    Agregar archivos de proyecto.

---
+++ TP4TiendaCafe/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TP4TiendaCafe
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TP4TiendaCafe/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TP4TiendaCafe
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
